fd535bf35f77032b40b6275bfe02d3ebed19579e
TarikFarhoud <127360345+TarikFarhoud@users.noreply.github.com>
2025-10-01 00:58:16 -0700

    Updated the unit tests to get 100 percent coverage by adding tests to check each grade A, B, C, D, and F and unit tests to check the map from GradeType to its associated string worked correctly

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -34,6 +34,38 @@ func TestGetGradeB(t *testing.T) {
 	}
 }
 
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 75, Assignment)
+	gradeCalculator.AddGrade("exam 1", 74, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 73, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 65, Assignment)
+	gradeCalculator.AddGrade("exam 1", 64, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 63, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
 func TestGetGradeF(t *testing.T) {
 	expected_value := "F"
 
@@ -47,5 +79,35 @@ func TestGetGradeF(t *testing.T) {
 
 	if expected_value != actual_value {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestStringMappingAssignment(t *testing.T) {
+	expected_string := "assignment"
+
+	actual_string := Assignment.String()
+
+	if expected_string != actual_string {
+		t.Errorf("Expected String to return '%s'; got '%s' instead", expected_string, actual_string)
+	}
+}
+
+func TestStringMappingExam(t *testing.T) {
+	expected_string := "exam"
+
+	actual_string := Exam.String()
+
+	if expected_string != actual_string {
+		t.Errorf("Expected String to return '%s'; got '%s' instead", expected_string, actual_string)
+	}
+}
+
+func TestStringMapping(t *testing.T) {
+	expected_string := "essay"
+
+	actual_string := Essay.String()
+
+	if expected_string != actual_string {
+		t.Errorf("Expected String to return '%s'; got '%s' instead", expected_string, actual_string)
 	}
 }
